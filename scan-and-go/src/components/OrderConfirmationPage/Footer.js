70e9a0273ff284b704cdee2f7c60a05cefe3cfaf
mam3fe6 <miguel_a_mendoza@homedepot.com>
2021-08-11 16:47:55 -0500

    added CSS

--- scan-and-go/src/components/OrderConfirmationPage/Footer.js
+++ scan-and-go/src/components/OrderConfirmationPage/Footer.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import '../../styles/orderConfirmation.scss';
 
 const extraOrderConfText = 'Your order details will be available in My Account in approximately 5 minutes. Scroll down to view an order summary and next steps related to your order.\n\nFor returns, please head to the Customer Service Desk.';
 
 const ForgetSomethingParagraph = () => (
-    <div style={{backgroundColor: '#F3F4F5', marginBottom: '25px', padding: '15px 10px 10px 10px'}}>
+    <div class='forget-something-paragraph'>
         {/* Top Row */}
-        <div style={{display: 'flex', justifyContent: 'space-between', marginBottom: '12px'}}>
-            <div style={{fontFamily: 'Helvetica Neue LT Pro', fontSize: '18px', lineHeight: '21px'}}>Forget Something?</div>
-            <div style={{fontSize: '14px', lineHeight: '21px', color: '#3E7697'}}>Keep Shopping</div>
+        <div class='top-row'>
+            <div class='forget-something-text'>Forget Something?</div>
+            <div class='keep-shopping-button'>Keep Shopping</div>
         </div>
         <div>
-            <div style={{color: '#3E7697', display: 'inline', fontSize: '14px', fontFamily: 'Helvetica Neue LT Pro', lineHeight: '19px'}}>Click Here </div>to add more items to your order before you exit the store.
+            <div class='click-here'>Click Here </div>to add more items to your order before you exit the store.
         </div>
         <br/>
         <div>
@@ -27,14 +28,14 @@ const OrderConfirmationInfo = ({orderInfo}) => {
     const orderTotal = 128.65;
     // Container
     return (
-        <div style={{marginBottom: '40px'}}>
+        <div class='order-conf-info-container'>
             {/* Items Info */}
-            <div style={{fontSize: '14px', lineHeight: '19px', marginBottom: '8px'}}>
+            <div class='items-info' >
                 {`Your order of ${formattedItems} is complete! A confirmation has been sent to ` }
                 <b>{`${userEmail}.`}</b>
             </div>
             {/* Receipt/Order Total */}
-            <div style={{fontSize: '18px', lineHeight: '21px', color: '#333333', fontFamily: 'Helvetica Neue LT Pro', marginBottom: '5px'}}>
+            <div class='receipt-order-total'>
                 <div>
                     {`Receipt #${receiptNumber}`}
                 </div>
@@ -43,7 +44,7 @@ const OrderConfirmationInfo = ({orderInfo}) => {
                 </div>
             </div>
             {/* Extra Footer Info */}
-            <div style={{fontSize: '14px', lineHeight: '19px', whiteSpace: 'pre-line'}}>
+            <div clas='extra-footer-info'>
                 {extraOrderConfText}
             </div>
         </div>
