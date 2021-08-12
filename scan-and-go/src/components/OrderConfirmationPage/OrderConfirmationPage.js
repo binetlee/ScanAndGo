5a3cdb03136afae97b489f4815ae040cec24d0c9
mam3fe6 <miguel_a_mendoza@homedepot.com>
2021-08-12 13:26:18 -0500

    renaming global receiptDetails

--- scan-and-go/src/components/OrderConfirmationPage/OrderConfirmationPage.js
+++ scan-and-go/src/components/OrderConfirmationPage/OrderConfirmationPage.js
@@ -51,12 +51,66 @@ const OrderConfQRContainer = ({payload}) => (
 );
 // 21x23.5px?
 export function OrderConfirmationPage() {
-    // const { state: metadataState } = useContext(MetadataContext);
+    const { state: metadataState, dispatch: metadataDispatch } = useContext(MetadataContext);
     const mockPayload = 'https://mocki.io/v1/029e8bd3-dce3-4cf4-a355-711783b907ae';//'https://webhook.site/829ea928-29b6-47e0-9dcc-affd0c3120d9';
     
-    // useEffect(() => {
-    //     console.log(`Metadata has changed to ${metadataState}`);
-    // }, [metadataState]);
+    useEffect(()=> {
+        metadataDispatch({	
+            type: 'UPDATE_RECEIPT_INFO',	
+            receiptDetails : {	
+                receiptId: 'W123452003',
+                receiptCreatedDate: "2020-03-21",
+                subTotal: 48.00,
+                salesTax : 3.00,
+                orderTotal : 51.00,
+                paymentType: {
+                    cardName : "family card",
+                    cardType : "PLCC",
+                    cashbackAmt : "2.56",
+                    lastFour : "4321"
+                },
+                billingAddress: {
+                  addressLine1: "3950 Spring Valley Rd",
+                  addressLine2: "Hackberry Creek",
+                  addressLine3: "",
+                  addressLine4: "",
+                  addressLine5: "",
+                  city: "Farmers Branch",
+                  dayPhone: "214-562-6856",
+                  emailId: "raju_datla@homedepot.com",
+                  firstName: "Raju",
+                  lastName: "Datla",
+                  middleName: "L",
+                  mobilePhone: "214-525-6366",
+                  state: "TX",
+                  zipCode: "75244"
+                },
+                lineItems: [
+                    {
+                        lineItemId: "101",
+                        upcCode: "8049393624",
+                        quantityOrdered: 5,
+                        itemDescription: "Hart 25 oz. Milled Face Hicory Farming Hammer",
+                        itemShortDescription: "Hart 25 oz. Hammer",
+                        itemCost: 4.25,
+                        totalCost: 21.25
+                    },
+                    {
+                        lineItemId: "102",
+                        upcCode: "72440143197",
+                        quantityOrdered: 5,
+                        itemDescription: "Meredith Home and Landscape Series Magazine",
+                        itemShortDescription: "Meredith Home and Landscape Series Magazine",
+                        itemCost: 4.25,
+                        totalCost: 26.75
+                    }
+                ]
+            }	
+        });
+    }, []);
+    useEffect(()=> {
+        console.log("metadata changed to:", metadataState.receiptDetails);
+    }, [metadataState]);
 
     return (
         <>
